5029139a829f6edb8b41fff652ddc759d6d57a1f
Felipe Marques <134336586+Felipe-Mrques@users.noreply.github.com>
2025-02-27 02:42:42 -0300

    Add files via upload

---
+++ curso_programacao/src/Exercicio2_5.java
@@ -0,0 +1,41 @@
+//*Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+//seguir, calcule e mostre o valor da conta a pagar
+
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class Exercicio2_5 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double total;
+
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+
+        if (codigo == 1){
+            total = quantidade * 4.0;
+        }
+        else if (codigo == 2){
+            total = quantidade * 4.5;
+        }
+        else if (codigo == 3){
+            total = quantidade * 5.0;
+        }
+        else if (codigo == 4){
+            total = quantidade * 2.0;
+        }
+        else {
+            total = quantidade * 1.5;
+        }
+
+        System.out.printf("TOTAL: R$ %.2f%n ", total);
+
+
+
+        sc.close();
+    }
+}
